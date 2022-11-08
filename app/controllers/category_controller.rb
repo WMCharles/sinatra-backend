4dbcadb9e72008ef4494d2517f82f3e9d82ec56d
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-11-08 14:06:16 -0500

    changes

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,1 +1,9 @@
-class 
+class CategoriesController < Sinatra::Base
+    get '/categories' do
+        "Category Index"
+      end
+    
+      get '/categories/:id' do
+        "Category #{params[:id]} Show"
+      end
+end
