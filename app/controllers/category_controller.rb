5eb81e14eeec0be367b21ec33ac84a0e3369ef23
Peris-Wambui <wangechi.wambui@student.moringaschool.com>
2022-11-08 20:09:06 -0500

    changes

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -7,3 +7,14 @@ class CategoriesController < Sinatra::Base
         "Category #{params[:id]} Show"
       end
 end
+# class Category < ApplicationController
+#     get '/categories' do
+#       @categories = Category.all
+#       erb :'categories/index'
+#     end
+  
+#     get '/categories/:category_slug' do
+#       @category = Category.find_by_slug(params[:category_slug])
+#       erb :'categories/show'
+#     end
+#   end
